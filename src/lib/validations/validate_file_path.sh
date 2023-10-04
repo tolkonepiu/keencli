93cf79d4ba9f711eadae06a511122914634ca8b4
Pavel Popov <popoffpavel@yandex.ru>
2023-10-04 15:34:56 +0700

    Fix linters errors

--- src/lib/validations/validate_file_path.sh
+++ src/lib/validations/validate_file_path.sh
@@ -1,6 +1,5 @@
 # shellcheck shell=bash
 validate_file_path() {
-       $test
   [[ -d "$1" ]] && echo "File cannot be a directory"
   [[ -d "$(dirname "$1")" ]] ||
     echo "File must be with an existing parent directory"
