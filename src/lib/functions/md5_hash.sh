fed1de689aab4eba160e208524a54c9ba50a62f1
Pavel Popov <me@popov.wtf>
2025-12-27 15:15:02 +0800

    Upgrade bashly to 1.3.5 and refactor HTTP parsing functions

--- src/lib/functions/md5_hash.sh
+++ src/lib/functions/md5_hash.sh
@@ -1,4 +1,7 @@
 # shellcheck disable=SC2154
 function md5_hash() {
-  echo -n "$1" | "${deps[md5]}" | awk '{print $1}'
+  local hash_output
+
+  hash_output=$(echo -n "$1" | "${deps[md5]}")
+  echo "${hash_output%% *}"
 }
--- src/lib/http/get_http_header_value.sh
+++ src/lib/http/get_http_header_value.sh
@@ -1,11 +1,15 @@
 function get_http_header_value() {
-  local key response
+  local key response line header_value
 
   key="${1:?header key must be set}"
   response="${2:?response must be set}"
 
-  echo "${response}" |
-    grep "${key}" |
-    awk -F ": " '{print $2}' |
-    tr -d '\r'
+  while IFS= read -r line; do
+    line="${line%$'\r'}"
+    if [[ "${line,,}" == "${key,,}:"* ]]; then
+      header_value="${line#*: }"
+      echo "${header_value}"
+      return 0
+    fi
+  done <<<"${response}"
 }
